3f5e0c53a3d04c74216b76494e8a7669d868a134
Lucca Tourinho <luccatourinho@gmail.com>
2024-11-30 20:26:35 -0300

    Design do Final finalizado

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -50,11 +50,11 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* Form */}
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-sky-950 rounded-xl">
               <h3 className="text-4xl text-accent">Here is how you contact me</h3>
               <p className="text-foreground/60">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem at iusto, nesciunt excepturi earum repellendus blanditiis fugiat ex explicabo officiis sunt rerum molestias eligendi quidem expedita corrupti eum odit nobis.
+                Interested in web development, API integration, or scaling your project? Let me know how I can assist you.
               </p>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -78,12 +78,44 @@ const Contact = () => {
                   </SelectGroup>
                 </SelectContent>
               </Select>
+
+              <Textarea 
+                className="h-[200px]"
+                placeholder="Write your message here."
+              />
+
+              <Button size={"md"} className="max-w-40">
+                Send Message
+              </Button>
             </form>
           </div>
             
           {/* Info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li 
+                    key={index}
+                    className="flex items-center gap-6"
+                  >
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-sky-950 text-accent rounded-xl flex items-center justify-center">
+                      <div className="text-[25px]">
+                        {item.icon}
+                      </div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-foreground/60">
+                        {item.title}
+                      </p>
+                      <h3 className="text-xl">
+                        {item.description}
+                      </h3>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         </div>
       </div>
